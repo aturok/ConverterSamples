5e841199acc63e4b778faa197f979db586bc909d
Alex Turok <a.o.turok@gmail.com>
2014-04-01 17:55:25 +0400

    Cleaned up the ItemToDescriptionConverter. Removed unused usings, introduced used ones and added constants

--- ConverterSamples/Converters/ItemToDescriptionConverter.cs
+++ ConverterSamples/Converters/ItemToDescriptionConverter.cs
@@ -1,21 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Globalization;
 using System.Windows.Data;
+
+using ConverterSamples.Resources;
 
 namespace ConverterSamples
 {
     public class ItemToDescriptionConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        private const string _DescriptonKeyFormat = "ItemDescription_{0}";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var item = (Models.Item)value;
-            var resourceKey = String.Format("ItemDescription_{0}", item.Kind);
-            return Resources.AppResources.ResourceManager.GetString(resourceKey, culture);
+            var resourceKey = String.Format(_DescriptonKeyFormat, item.Kind);
+            return AppResources.ResourceManager.GetString(resourceKey, culture);
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
